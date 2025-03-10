81052e340c37e212c3e840718a319b03314c2fa2
Claire <winnieyuan1004@gmail.com>
2025-03-10 22:26:24 +0000

    Add Google Maps API for location recommendations

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import App from './weather';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
-
     <div className="weather">
-    <App />
+      <App />
     </div>
 
     <div className="stars">
-    <Stars />
+      <Stars />
     </div>
-    
+
   </React.StrictMode>
 );
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAP_API_KEY}&libraries=places`;
+script.async = true;
+document.head.append(script);
