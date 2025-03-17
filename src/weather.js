7a392e70b3dd8eaf93516db87dc89c16c4ed303e
Xiaowen Yuan <116190605+CXWYuan@users.noreply.github.com>
2025-03-17 23:10:16 +0100

    Add sidebar and styles (#26)

--- src/weather.js
+++ src/weather.js
@@ -8,6 +8,7 @@ function Weather() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
   const [locationRecommendations, setLocationRecommendations] = useState([]);
+  const [showSidebar, setShowSidebar] = useState(false);
   const API_KEY = process.env.REACT_APP_API_KEY;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
 
@@ -71,11 +72,29 @@ function Weather() {
   }
 
   return (
-    <div className="App">
+    <div className="App" onClick={() => setShowSidebar(false)}>
+
       <div className="top_bar">
+        {
+          showSidebar &&
+          <div id='sidebar'>
+            <div className='sidebar-items' onClick={() => window.location = "/"}>
+              Locations
+            </div>
+            <div className='sidebar-items' onClick={() => window.location = "/events"}>
+              Upcoming Celestial Events
+            </div>
+            <div className='sidebar-items' onClick={() => window.location = "/"}>
+              Recommended Spots
+            </div>
+          </div>
+        }
 
         <div id="menu_bar">
-          <svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg id="sidebar_toggle" width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={(e) => {
+            e.stopPropagation()
+            setShowSidebar(true)
+          }}>
             <path fill-rule="evenodd" clip-rule="evenodd" d="M39.4167 12.5417V8.95833H3.58333V12.5417H39.4167ZM39.4167 19.7083V23.2917H3.58333V19.7083H39.4167ZM39.4167 30.4583V34.0417H3.58333V30.4583H39.4167Z" fill="white" fill-opacity="0.6" />
           </svg>
         </div>
@@ -102,7 +121,7 @@ function Weather() {
 
 
 
-        <div className="weather_container">
+        <div className="weather_container" >
 
 
           <div className="conditions weather_element">
@@ -114,7 +133,7 @@ function Weather() {
               level="Good"
             />
           </div>
-            
+
 
           <div className="location weather_element">
             <SmallWidget
@@ -140,7 +159,7 @@ function Weather() {
               level="ISS Pass Overhead"
             />
           </div>
-          
+
 
 
           <div className="general_weather weather_element">
@@ -152,8 +171,7 @@ function Weather() {
               }
               level={
                 data.main || data.weather || data.wind
-                  ? `${data.main ? `${data.main.temp.toFixed()}°C` : ""} ${
-                      data.weather ? data.weather[0].main : ""
+                  ? `${data.main ? `${data.main.temp.toFixed()}°C` : ""} ${data.weather ? data.weather[0].main : ""
                     } ${data.wind ? `${data.wind.speed.toFixed()} km/h` : ""}`.trim()
                   : "No weather data"
               }
@@ -189,25 +207,25 @@ function Weather() {
 
 
           <div className="humidity weather_element">
-          <SmallWidget
-            title="HUMIDITY"
-            icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <path d="M12 22C13.8565 22 15.637 21.2625 16.9497 19.9497C18.2625 18.637 19 16.8565 19 15C19 13 18 11.1 16 9.5C14 7.9 12.5 5.5 12 3C11.5 5.5 10 7.9 8 9.5C6 11.1 5 13 5 15C5 16.8565 5.7375 18.637 7.05025 19.9497C8.36301 21.2625 10.1435 22 12 22Z" stroke="white" stroke-opacity="0.6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>}
-            level="90%"
-          />
+            <SmallWidget
+              title="HUMIDITY"
+              icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path d="M12 22C13.8565 22 15.637 21.2625 16.9497 19.9497C18.2625 18.637 19 16.8565 19 15C19 13 18 11.1 16 9.5C14 7.9 12.5 5.5 12 3C11.5 5.5 10 7.9 8 9.5C6 11.1 5 13 5 15C5 16.8565 5.7375 18.637 7.05025 19.9497C8.36301 21.2625 10.1435 22 12 22Z" stroke="white" stroke-opacity="0.6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>}
+              level="90%"
+            />
           </div>
 
-          
+
           <div className="light_pollution weather_element">
-          <SmallWidget
-            title="LIGHT POLLUTION"
-            icon={<svg xmlns="http://www.w3.org/2000/svg" width="18" height="26" viewBox="0 0 18 26" fill="none">
-              <path d="M13 15.2222C13.25 14.1111 13.875 13.3334 14.875 12.4445C16.125 11.4445 16.75 10 16.75 8.55558C16.75 6.78747 15.9598 5.09178 14.5533 3.84154C13.1468 2.59129 11.2391 1.88892 9.25 1.88892C7.26088 1.88892 5.35322 2.59129 3.9467 3.84154C2.54018 5.09178 1.75 6.78747 1.75 8.55558C1.75 9.66669 2 11 3.625 12.4445C4.5 13.2222 5.25 14.1111 5.5 15.2222M5.5 19.6667H13M6.75 24.1111H11.75" stroke="white" stroke-opacity="0.6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>}
-            level="High"
-          />
-        </div>
+            <SmallWidget
+              title="LIGHT POLLUTION"
+              icon={<svg xmlns="http://www.w3.org/2000/svg" width="18" height="26" viewBox="0 0 18 26" fill="none">
+                <path d="M13 15.2222C13.25 14.1111 13.875 13.3334 14.875 12.4445C16.125 11.4445 16.75 10 16.75 8.55558C16.75 6.78747 15.9598 5.09178 14.5533 3.84154C13.1468 2.59129 11.2391 1.88892 9.25 1.88892C7.26088 1.88892 5.35322 2.59129 3.9467 3.84154C2.54018 5.09178 1.75 6.78747 1.75 8.55558C1.75 9.66669 2 11 3.625 12.4445C4.5 13.2222 5.25 14.1111 5.5 15.2222M5.5 19.6667H13M6.75 24.1111H11.75" stroke="white" stroke-opacity="0.6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>}
+              level="High"
+            />
+          </div>
 
 
 
