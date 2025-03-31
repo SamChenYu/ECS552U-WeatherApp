761392f3041ebf09c653c1a714a418544e43eed8
Sam <samchenyu@gmail.com>
2025-04-01 00:27:40 +0100

    Changed light/dark mode icons

--- src/weather.js
+++ src/weather.js
@@ -179,39 +179,40 @@ function Weather({ isDarkMode, toggleDarkMode }) {
 
             {isDarkMode ? (
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="white"
-                fillOpacity="0.6"
-                className="lightbulb"
-                viewBox="0 0 16 16"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  toggleDarkMode()
-                }}
-              >
-                <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1" />
-              </svg>
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 25 25"
+              width="50px"
+              height="50px"
+              color ="white"
+              fillOpacity = "0.6"
+              onClick={(e) => {
+                e.stopPropagation();
+                toggleDarkMode()
+              }}
+            >
+              <path
+                fill="currentColor"
+                d="m3.55 19.09l1.41 1.41l1.8-1.79l-1.42-1.42M12 6c-3.31 0-6 2.69-6 6s2.69 6 6 6s6-2.69 6-6c0-3.32-2.69-6-6-6m8 7h3v-2h-3m-2.76 7.71l1.8 1.79l1.41-1.41l-1.79-1.8M20.45 5l-1.41-1.4l-1.8 1.79l1.42 1.42M13 1h-2v3h2M6.76 5.39L4.96 3.6L3.55 5l1.79 1.81zM1 13h3v-2H1m12 9h-2v3h2"
+              ></path>
+            </svg>
             ) : (
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="white"
-                fillOpacity="0.6"
-                className="lightbulb"
-                viewBox="0 0 16 16"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  toggleDarkMode()
-                }}
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2.23 4.35A6 6 0 0 0 2 6c0 1.691.7 3.22 1.826 4.31.203.196.359.4.453.619l.762 1.769A.5.5 0 0 0 5.5 13a.5.5 0 0 0 0 1 .5.5 0 0 0 0 1l.224.447a1 1 0 0 0 .894.553h2.764a1 1 0 0 0 .894-.553L10.5 15a.5.5 0 0 0 0-1 .5.5 0 0 0 0-1 .5.5 0 0 0 .288-.091L9.878 12H5.83l-.632-1.467a3 3 0 0 0-.676-.941 4.98 4.98 0 0 1-1.455-4.405zm1.588-2.653.708.707a5 5 0 0 1 7.07 7.07l.707.707a6 6 0 0 0-8.484-8.484zm-2.172-.051a.5.5 0 0 1 .708 0l12 12a.5.5 0 0 1-.708.708l-12-12a.5.5 0 0 1 0-.708"
-                />
-              </svg>
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 25 25"
+              width="50px"
+              height="50px"
+              color ="white"
+              fillOpacity = "0.6"
+              onClick={(e) => {
+                e.stopPropagation();
+                toggleDarkMode()
+              }}
+            >
+              <path
+                fill="currentColor"
+                d="M2 12a10 10 0 0 0 13 9.54a10 10 0 0 1 0-19.08A10 10 0 0 0 2 12"
+              ></path>
+            </svg>
             )}
           </div>
         </div>
